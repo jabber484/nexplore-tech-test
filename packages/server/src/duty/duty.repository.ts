8facbb1501db94dfb25fb450b92d383053cb4689
Ma Kwan Lok <jabber484@gmail.com>
2023-12-12 11:27:08 +0800

    feat(server): remove findAll default limit

--- packages/server/src/duty/duty.repository.ts
+++ packages/server/src/duty/duty.repository.ts
@@ -1,11 +1,9 @@
 import { client } from "../infras/database"
 import { CreateDutyRequest, Duty } from "./duty.entity"
 
-const FIND_ALL_LIMIT = 50
-
 export const findAll = async (skip?: number, limit?: number): Promise<Array<Duty>> => {
   const text = 'SELECT id,name FROM duty ORDER BY id DESC OFFSET $1 LIMIT $2'
-  const values = [skip ? skip : 0, limit ? limit : FIND_ALL_LIMIT]
+  const values = [skip ? skip : 0, limit ? limit : "ALL"]
 
   const res = await client.query(text, values)
   return res.rows
