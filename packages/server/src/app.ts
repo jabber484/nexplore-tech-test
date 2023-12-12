0c759e79cc657828eed5cfe155c6e355b1e05bca
Ma Kwan Lok <jabber484@gmail.com>
2023-12-12 11:17:53 +0800

    feat(server): add cors

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import { dutyController } from "./duty";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { errorHandler } from "./error/errorHandler";
 
 export const startWebServer = () => {
   const app: Express = express();
+  app.use(cors())
   app.use(bodyParser.json())
   app.use("/duty", dutyController)
   app.use(errorHandler)
